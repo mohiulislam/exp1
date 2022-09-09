aaaee4d8ac9483a6050269eb8badb329641871f2
Mohiul Islam <mohiulislam900@gmail.com>
2022-09-10 03:15:16 +0600

    comment added

--- prac.js
+++ prac.js
@@ -33,10 +33,14 @@
 // }
 // console.dir(Person);
 
+
+
+//for going page2
 document.querySelector("#button_of_page1").onclick=function () {
     location.pathname="/page2.html";
 }
 
+//for going page1
 document.querySelector("#button_of_page2").onclick=function () {
     location.pathname="/page1.html";
 }
