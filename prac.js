5245d8a2fc0b4a1fe2cf138214bd1b81c38563fe
Mohiul Islam <mohiulislam900@gmail.com>
2022-09-10 13:33:18 +0600

    .......

--- prac.js
+++ prac.js
@@ -1,40 +1,3 @@
-// function af(a,b) {
-//   return {num : a,
-// roll:b};
-// }
-// var b=af;
-// console.log(obj=af(66,77));
-
-// var obj=Object.create(b(111,333));
-// var obj2=Object.create(b(222,444));
-
-// console.dir(obj);
-// console.dir(obj2);
-
-// function Fruit(color, taste, seeds) {
-//   this.color = color;
-//   this.taste = taste;
-//   this.seeds = seeds;
-// }
-
-// const fruit1 = new Fruit('Yellow', 'Sweet', 1);
-
-// console.dir(fruit1);
-// console.dir(Fruit.prototype);
-// function Person(name, age, location, occupation){
-//     this.name = name
-//     this.age = age
-//     this.location = location
-//     this.occupation = occupation
-
-//     this.printDetails = function(){
-//         console.log(`My name is ${this.name} and I'm ${this.age} years old. I live in ${this.location} and I work as a ${this.occupation}.`);
-//     };
-// }
-// console.dir(Person);
-
-
-
 //for going page2
 document.querySelector("#button_of_page1").onclick=function () {
     location.pathname="/page2.html";
@@ -44,6 +7,3 @@ document.querySelector("#button_of_page1").onclick=function () {
 document.querySelector("#button_of_page2").onclick=function () {
     location.pathname="/page1.html";
 }
-
-
-
